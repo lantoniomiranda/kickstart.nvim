90cb7cb7ae8a2ce576fbf2fbaeaeeffecfd4f114
Luís Miranda <100844405+lantoniomiranda@users.noreply.github.com>
2026-05-16 10:54:07 +0100

    added claude code integration

--- init.lua
+++ init.lua
@@ -846,6 +846,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        sql = { 'sqlfmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
